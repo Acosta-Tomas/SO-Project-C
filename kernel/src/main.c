6eb0f85979fce1531618ef565f6f675cfaff0c17
Acosta-Tomas <tomasacosta@frba.utn.edu.ar>
2024-04-19 10:15:08 -0300

    Commit inicial probando tp0 en kernel con threads

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -2,7 +2,97 @@
 #include <stdio.h>
 #include <utils/hello.h>
 
+void servidor_main(t_config*);
+void cliente_main(t_config*);
+
 int main(int argc, char* argv[]) {
+    t_config* config = config_create("kernel.config");
+
+    if (config == NULL) exit(EXIT_FAILURE); 
+
     decir_hola("Kernel");
-    return 0;
+    
+    pthread_t  serveridor_thread, cliente_thread;
+
+    // Crear el hilo del servidor
+    if (pthread_create(&serveridor_thread, NULL, (void*) servidor_main, config)) {
+        perror("Error creating server thread");
+        exit(EXIT_FAILURE);
+    }
+
+    // Esperar un poco para que el servidor esté listo antes de iniciar el cliente
+    sleep(1);
+
+    // Crear el hilo del cliente
+    if (pthread_create(&cliente_thread, NULL, (void*) cliente_main, config)) {
+        perror("Error creating client thread");
+        exit(EXIT_FAILURE);
+    }
+
+    // Esperar a que ambos hilos terminen
+    pthread_join(serveridor_thread, NULL);
+    pthread_join(cliente_thread, NULL);
+
+    config_destroy(config);
+    return EXIT_SUCCESS;
+}
+
+// Probando poder crear un server que escuche como el tp0 en un thread (esta funcion la pasaria a otro archivo server.c);
+void servidor_main(t_config* config) {
+    t_log* logger = log_create("kernel.logs", config_get_string_value(config, "LOGGER_SERVIDOR"), true, LOG_LEVEL_INFO);
+
+    log_info(logger, "Thread Servidor creado exitosamente");
+
+    int server_fd = iniciar_servidor(config_get_string_value(config, "PUERTO_ESCUCHA"));
+    log_info(logger, "Servidor listo para recibir al cliente");
+
+    int cliente_fd = esperar_cliente(server_fd);
+    log_info(logger, "Se conecto un cliente");
+
+    t_list* lista;
+	
+    while (1) {
+		int cod_op = recibir_operacion(cliente_fd);
+		switch (cod_op) {
+		case MENSAJE:
+			recibir_mensaje(cliente_fd, logger);
+			break;
+		case PAQUETE:
+			lista = recibir_paquete(cliente_fd);
+			log_info(logger, "Me llegaron los siguientes valores:\n");
+			list_iterate(lista, (void*) iterator);
+			break;
+		case -1:
+			log_error(logger, "el cliente se desconecto. Terminando servidor");
+            log_destroy(logger);
+			exit(EXIT_FAILURE);
+		default:
+			log_warning(logger,"Operacion desconocida. No quieras meter la pata");
+			break;
+		}
+	}
+    log_destroy(logger);
+}
+
+// Probando poder crear un cliente que mande info como el tp0 en un thread (esta funcion la pasaria a otro archivo cliente.c);
+void cliente_main(t_config* config){
+    t_log* logger = log_create("kernel.logs", config_get_string_value(config, "LOGGER_CLIENTE"), true, LOG_LEVEL_INFO);
+    log_info(logger, "Thread Cliente creado exitosamente");
+
+    char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+    char* ip_cpu = config_get_string_value(config, "IP_CPU");
+    char* puerto_cpu_dispatch = config_get_string_value(config, "PUERTO_CPU_DISPATCH");
+    char* puerto_cpu_interrupt = config_get_string_value(config, "PUERTO_CPU_INTERRUPT");
+
+    // Revisar pporque siempre me tira warnings con esto!
+    log_info(logger, ip_memoria);
+    log_info(logger, puerto_memoria);
+    log_info(logger, ip_cpu);
+    log_info(logger, puerto_cpu_dispatch);
+    log_info(logger, puerto_cpu_interrupt);
+
+    leer_consola(logger);
+
+    log_destroy(logger);
 }
