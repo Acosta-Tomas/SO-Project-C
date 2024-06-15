bb46fbe0a0f9065f0f1899f1e5402b1d584ad8a6
Acosta-Tomas <tomasacosta@frba.utn.edu.ar>
2024-06-15 17:39:06 -0300

    Fix(KERNEL): Exit process without test

--- kernel/src/exit_proceso.c
+++ kernel/src/exit_proceso.c
@@ -2,29 +2,28 @@
 
 void finalizar_proceso(t_pcb* pcb){ 
     pthread_t exit_thread;
-    uint32_t pid = pcb->pid;
     
-    if (pthread_create(&exit_thread, NULL, memoria_finalizar_proceso, &pid)) {
+    if (pthread_create(&exit_thread, NULL, memoria_finalizar_proceso, pcb)) {
         log_error(logger, "No se pudo crear thread para finilzar proceso");
         exit(EXIT_FAILURE);
     } else {
         pthread_detach(exit_thread);
     }
 
-    free(pcb->registers);
-    free(pcb);
-
     sem_post(&cont_multi);
 }
 
-void* memoria_finalizar_proceso(void* pid){
-    uint32_t* pid_end = (uint32_t*) pid;
+void* memoria_finalizar_proceso(void* pcb){
+    t_pcb* pcb_end = (t_pcb*) pcb;
     op_code code = END_PID;
 
-    log_info(logger, "Enviar PID: %u a finalizar", *(pid_end));
+    log_info(logger, "Enviar PID: %u a finalizar", pcb_end->pid);
 
     send(memoria_fd, &code, sizeof(uint32_t), 0);
-    send(memoria_fd, pid_end, sizeof(uint32_t), 0);
+    send(memoria_fd, &pcb_end->pid, sizeof(uint32_t), 0);
+
+    free(pcb_end->registers);
+    free(pcb_end);
 
     return EXIT_SUCCESS;
 }
